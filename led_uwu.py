8d876be0f7a72e9db230bc7b3d064c1863d2df56
= <=>
2025-07-01 14:40:38 +0100

    sdfghjkl;

--- led_uwu.py
+++ led_uwu.py
@@ -1,9 +1,12 @@
 import os
-while True:
-    turnon = input("Enter on or off here: ")
 
-    if turnon.lower() == "z":
+while True:
+    ledstatus = input("z is on x is off ")
+
+    if ledstatus == "z":
         os.system("echo 1 | sudo tee /sys/class/leds/ACT/brightness")
-    elif turnon.lower() == "x":
+    elif ledstatus == "x":
         os.system("echo 0 | sudo tee /sys/class/leds/ACT/brightness")
+
+
    
