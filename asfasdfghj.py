8a6f73b6cb06f2df7218353a7f6ef16a790efb41
= <=>
2025-07-02 11:44:31 +0100

    asd

---
+++ asfasdfghj.py
@@ -0,0 +1,32 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+
+# # message
+# message = "... --- ... ..- / ..-- .- --."
+
+# # show on leds
+# while True:
+
+
+#     sense.show_message(message,00.05, text_colour = [255,255,255], back_colour= [255,0,0])
+
+# # print lol
+
+# print(message)
+
+# sense.clear()
+
+# num1 = float(input("whats ur first number lol:" ))
+# num2 = float(input("whats ur second number lol:" ))
+
+# result = num1 ** num2
+
+# print(f"you have {result} lol")
+
+# sense.show_message(f"you have {result}")
+
+
+
+
+
+
